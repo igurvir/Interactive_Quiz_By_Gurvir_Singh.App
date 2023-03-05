266cd7191b0479484c36aad7e0396152d0a4303f
Gurvir Singh <gurvirsingh0504@gmail.com>
2023-03-05 23:35:52 +0500

    Added Option button functionality and right/ wrong answer color change functionality

--- QuizViewModel.cs
+++ QuizViewModel.cs
@@ -17,6 +17,11 @@ namespace Interactive_Quiz_By_Gurvir_Singh
         private ICommand _Option2Command;
         private ICommand _Option3Command;
         private ICommand _Option4Command;
+        private Color _color1 = Colors.LightGray;
+        private Color _color2 = Colors.LightGray;
+        private Color _color3 = Colors.LightGray;
+        private Color _color4 = Colors.LightGray;
+        private bool _isEnabled = true;
         public QuizViewModel()
         {
             _quiz = new Quiz();
@@ -54,6 +59,26 @@ namespace Interactive_Quiz_By_Gurvir_Singh
         {
             get { return !string.IsNullOrEmpty(CurrentQuestion.Option3) && !string.IsNullOrEmpty(CurrentQuestion.Option4) ? true : false; }
         }
+        public bool isEnabled
+        {
+            get { return _isEnabled; }
+        }
+        public Color color1
+        {
+            get { return _color1; }
+        }
+        public Color color2
+        {
+            get { return _color2; }
+        }
+        public Color color3
+        {
+            get { return _color3; }
+        }
+        public Color color4
+        {
+            get { return _color4; }
+        }
         public MultipleChoiceQuestion CurrentQuestion
         {
             get { return (MultipleChoiceQuestion)_quiz._currentQuestion; }
@@ -86,20 +111,65 @@ namespace Interactive_Quiz_By_Gurvir_Singh
         public event PropertyChangedEventHandler PropertyChanged;
         public void Option1_Clicked()
         {
+            if (_quiz.CheckAnswer(CurrentQuestion.Option1))
+            {
+                _color1 = Colors.Green;
+            }
+            else
+            {
+                _color1 = Colors.Red;
+            }
+            _isEnabled = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color1"));
         }
         public void Option2_Clicked()
         {
+            if (_quiz.CheckAnswer(CurrentQuestion.Option1))
+            {
+                _color2 = Colors.Green;
+            }
+            else
+            {
+                _color2 = Colors.Red;
+            }
+            _isEnabled = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color2"));
         }
         public void Option3_Clicked()
         {
+            if (_quiz.CheckAnswer(CurrentQuestion.Option1))
+            {
+                _color3 = Colors.Green;
+            }
+            else
+            {
+                _color3 = Colors.Red;
+            }
+            _isEnabled = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color3"));
         }
         public void Option4_Clicked()
         {
+            if (_quiz.CheckAnswer(CurrentQuestion.Option1))
+            {
+                _color4 = Colors.Green;
+            }
+            else
+            {
+                _color4 = Colors.Red;
+            }
+            _isEnabled = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color4"));
         }
         private void NextQuestion()
         {
             _quiz._currentQuestion = _quiz.GetNextQuestion();
-
+            ResetColor();
+            _isEnabled = true;
             NotifyPropertyChanged(nameof(CurrentQuestion));
             if (string.IsNullOrEmpty(CurrentQuestion.Option3) && string.IsNullOrEmpty(CurrentQuestion.Option4))
             {
@@ -118,10 +188,22 @@ namespace Interactive_Quiz_By_Gurvir_Singh
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Option4"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("buttonDisable"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color1"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color2"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color3"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("color4"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isEnabled"));
         }
         private async void QuitQuiz()
         {
             await Application.Current.MainPage.DisplayAlert("Result", "You got " + _quiz.CorrectCount + " correct answers out of " + _quiz._UserquestionCount + "", "OK");
+        }
+        public void ResetColor()
+        {
+            _color1 = Colors.LightGray;
+            _color2 = Colors.LightGray;
+            _color3 = Colors.LightGray;
+            _color4 = Colors.LightGray;
         }
         private void NotifyPropertyChanged(string propertyName)
         {
