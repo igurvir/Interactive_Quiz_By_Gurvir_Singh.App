25a55e79e45ead8b804d0b6363a0ebf74a8ca01c
Gurvir Singh <gurvirsingh0504@gmail.com>
2023-03-05 21:37:59 +0500

    Added Question Classes and Quiz class

---
+++ TrueFalseQuestion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Quiz_By_Gurvir_Singh
+{
+    public class TrueFalseQuestion:Question
+    {
+    }
+}
